b50ca3f0614e0664ab0f8edc6a7b160a25062461
Thomas Nield <Thomas.Nield@wnco.com>
2018-11-01 11:40:59 -0500

    snapshot build fixes

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -6,9 +6,13 @@ buildTag="$TRAVIS_TAG"
 
 export GRADLE_OPTS=-Xmx1024m
 
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   ./gradlew -PreleaseMode=pr build
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+  ./gradlew -PreleaseMode=branch -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" build --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   ./gradlew -PreleaseMode=full -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" build --stacktrace
