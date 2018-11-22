1f805ab3390f47b9d33886007a63357ea9d95bb8
Thomas Nield <thomasnield@live.com>
2018-11-22 08:59:38 -0600

    try to create pom manually

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -3,7 +3,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew build
+  ./gradlew -x test build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   ./gradlew -x test -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" build bintrayUpload -PreleaseMode=full --stacktrace --info
